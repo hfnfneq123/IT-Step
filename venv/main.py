5578542484fd050a1eecd2db08d09cb607643452
admin <agavrilchenko2009@gmail.com>
2025-10-11 14:56:43 +0100

    Class and attributes3

--- venv/main.py
+++ venv/main.py
@@ -1,2 +1,32 @@
+class Human:
+    def __init__(self, name):
+        self.name = name
 
+
+class Auto:
+    def __init__(self, brand):
+        self.brand = brand
+        self.passengers = []
+
+    def add_passenger(self, *args):
+        for passenger in args:
+            self.passengers.append(passenger)
+
+    def print_passengers_names(self):
+        if self.passengers != []:
+            print(f"Names of {self.brand} passengers:")
+            for passenger in self.passengers:
+                print(passenger.name)
+        else:
+            print(f"There are no passengers in {self.brand}")
+
+
+alex = Human("Alex")
+anna = Human("Anna")
+
+car = Auto("Mersedes")
+
+car.add_passenger(alex, anna)
+
+car.print_passengers_names()
 
